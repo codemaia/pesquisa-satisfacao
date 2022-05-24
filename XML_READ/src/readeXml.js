49b815032bfcd66d86f2456534466b1d732bf72f
Renan Maia <72897781+codemaia@users.noreply.github.com>
2022-05-24 17:34:23 -0300

    Update readeXml.js

--- XML_READ/src/readeXml.js
+++ XML_READ/src/readeXml.js
@@ -5,7 +5,13 @@ const {
     getIdentificacaoTransacao,
     getOrigem,
     getPadrao, 
-    getLoginSenhaPrestador
+    getLoginSenhaPrestador,
+    getDestino,
+    getDadosProtocolo,
+    getRelacaoGuiasArrays,
+    getDadosGuiasArray,
+    getDetalhesGuia,
+    getDetalhesGuiaArray
 } = require('./parserObjectXml');
 
 const ans = "ans:"
@@ -21,16 +27,26 @@ async function getDataXml() {
     const jsonXml = await readXml("data.xml");
 
     const cabecalho = arrayToObject(jsonXml[`${ans}mensagemTISS`][`${ans}cabecalho`])
-    console.log(cabecalho)
+    //console.log(cabecalho)
     const identificacaoTransacao = getIdentificacaoTransacao(cabecalho[`${ans}identificacaoTransacao`])
     const origem = getOrigem(cabecalho[`${ans}origem`])
     const padrao = getPadrao(cabecalho[`${ans}Padrao`])
     const loginSenhaPrestador = getLoginSenhaPrestador(cabecalho[`${ans}loginSenhaPrestador`])
+    const destino = getDestino(cabecalho[`${ans}destino`])
+    //
+    const operadoraParaPrestador = arrayToObject(jsonXml[`${ans}mensagemTISS`][`${ans}operadoraParaPrestador`])
+    const demonstrativosRetorno = arrayToObject(operadoraParaPrestador[`${ans}demonstrativosRetorno`]);
+    const dadosProtocolo = getDadosProtocolo(demonstrativosRetorno[`${ans}demonstrativoAnaliseConta`]);
+    const relacaoGuiasArrays = getRelacaoGuiasArrays(dadosProtocolo.relacaoGuiasArray);
+    const dadosGuiasArray = getDadosGuiasArray(relacaoGuiasArrays);
+    const detalhesGuia = getDetalhesGuia(dadosGuiasArray);
+    
+
 
     // for (const iterator of getListaGuias(cabecalho[`${ans}DetalhesGuia`])) {
     //     iterator
     // }
-    return { origem, identificacaoTransacao, padrao, loginSenhaPrestador }
+    return { origem, identificacaoTransacao, padrao, loginSenhaPrestador, destino, demonstrativosRetorno, relacaoGuiasArrays, dadosGuiasArray, detalhesGuia }
 }
 
 async function readXml(fileName) {
